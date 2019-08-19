6fff1c5ad70b5ba25e7de0d2e1d34e092c359ff9
Wonpyoung <cwp10@msn.com>
2019-08-19 11:05:47 +0900

    유니티 패키지로 생성하는 에디터 스크립트 제작

---
+++ Assets/IVRCamera3D/Editor/IVRPluginPackager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+
+public class IVRPluginPackager
+{
+    [MenuItem("Window/Packager/Create IVRCamera Plugin")]
+    public static void Export()
+    {
+        AssetDatabase.ExportPackage(
+            new string[] {"Assets/IVRCamera3D"},
+            "IVRPlugin.unitypackage",
+            ExportPackageOptions.Recurse
+        );
+    }
+}
